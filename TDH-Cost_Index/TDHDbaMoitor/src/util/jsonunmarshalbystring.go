4ab805afe7b0e74c9cb284c3675094a3c8d8fb89
AlexHU421 <31975427+AlexHU421@users.noreply.github.com>
2021-09-23 17:05:38 +0800

    Create jsonunmarshalbystring.go

---
+++ TDH-Cost_Index/TDHDbaMoitor/src/util/jsonunmarshalbystring.go
@@ -0,0 +1,15 @@
+package util
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func JsonUnmarshalByString(str string )map[string]interface{} {
+	mMap := make (map[string]interface{})
+	err := json.Unmarshal([]byte(str),&mMap)
+	if err !=nil {
+		fmt.Println(err)
+	}
+	return mMap
+}
