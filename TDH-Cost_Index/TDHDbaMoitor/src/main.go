7becdc608f2c6ef47827de65cce3f650ac19dc68
AlexHU421 <31975427+AlexHU421@users.noreply.github.com>
2021-09-28 18:43:16 +0800

    Update main.go

--- TDH-Cost_Index/TDHDbaMoitor/src/main.go
+++ TDH-Cost_Index/TDHDbaMoitor/src/main.go
@@ -11,7 +11,7 @@ import (
 const(
 	dbaurl=	"https://tdh3:4040/api/inceptor/"
 	dataKey=	"?dataKey="
-	serverurl = "https://tdh2:4040/api/inceptor/servers"
+	serverurl = "https://tdh3:4040/api/inceptor/servers"
 	queriesurl = dbaurl + "sqls" + dataKey
 	querysurl= dbaurl + "sql" + dataKey
 	stagetsurl = dbaurl + "stage" + dataKey
@@ -31,71 +31,114 @@ type MyJsonName struct {
 	WatchmanTimestamp int64         `json:"watchmanTimestamp"`
 }
 
+func crawserverpage (c chan map[int]entity.JsonServer){
+	for {
+	dataType, _ := json.Marshal(util.JsonUnmarshalByString(util.CrawlPage(serverurl, token))["data"])
+	c <- entity.MapByJson(string(dataType))
+		time.Sleep(time.Minute)
+	}
+}
+func crawquerypage (cquerymap chan map[string]entity.Query,serverKey string){
+	for {
+		serverqueryurl := queriesurl + serverKey + "&dataSize=100"
+		var queies entity.JsonQuery1
+		err := json.Unmarshal([]byte(util.CrawlPage(serverqueryurl, token)), &queies)
+		if err != nil {
+			panic(err)
+		}
+		cquerymap <- entity.GetQueriesList(queies)
+		time.Sleep(time.Second*5)
+	}
+}
 
 
 func main() {
-	now := time.Now().UnixNano()
 
-	//tiker := time.NewTicker(time.Second*5)
-	//for i:=1; i>0 ;i++{
-	//	i=1
-
-	fmt.Println(util.UnixMillTime(now))
-	//var xm MyJsonName
-	//err := json.Unmarshal([]byte(util.CrawlPage(url,token)), &xm)
-	//if err != nil{
-	//	panic(err)
-	//}
-	//fmt.Println(xm.Data)
-	//fmt.Println(util.CrawlPage(url,token))
-
-	dataType, _ := json.Marshal(util.JsonUnmarshalByString(util.CrawlPage(serverurl, token))["data"])
+	now := util.UnixMillTime(time.Now().UnixNano())
+	fmt.Println(now)
 	//Get lastserver map info    后续切片并发需存入redis做并发访问池
-	servermap := entity.MapByJson(string(dataType))
+	cservermap :=make(chan map[int]entity.JsonServer)
 
-	//Test look
-	for serverid, serverinfo := range servermap {
-		fmt.Println(serverid, serverinfo.DataKey)
-	}
+	//爬server列表（只保留最近一次启动）
+	go 	crawserverpage(cservermap)
 
 	//Test  query数据量太大，需定时器定时获取
-	serverqueryurl := queriesurl + "Inceptor::tdh2::7fd73b47-8aec-4668-951e-8c8a72255832" + "&dataSize=10"
-	//fmt.Println(serverqueryurl)
-	//fmt.Println(util.CrawlPage(serverqueryurl,token))
+	cquerymap :=make(chan map[string]entity.Query)
+	//爬query列表（增量map）
 
-	//Get querymap info    后续切片并发需存入redis做并发访问池
-	querymap := make(map[string]entity.Query)
-	var queies entity.JsonQuery1
-	err := json.Unmarshal([]byte(util.CrawlPage(serverqueryurl, token)), &queies)
-	if err != nil {
-		panic(err)
+	go func(){
+		for {
+			fmt.Println(time.Now())
+			for _, serverinfo := range <-cservermap {
+			go crawquerypage(cquerymap,serverinfo.DataKey)
+			}
+			time.Sleep(time.Second*5)
+		}}()
+
+
+	//querymaps := make(chan map[string]entity.Query)
+	querymaps := make(map[string]entity.Query)
+	go func() {
+		for {
+			querymap := <- cquerymap
+			//querysmap :=<- querymaps
+			//fmt.Println("look", <-cquerymap)
+			for k,v :=range querymap {
+					querymaps[k]=v
+			}
+			for k,v :=range querymaps {
+
+				if 	util.FilterByUnixtime(v.SubmissionTime,5,"minute") {
+					delete(querymaps,k)
+				}
+					fmt.Println(k,v)
+			}
+			fmt.Println(len(querymaps))
+			time.Sleep(time.Second * 5)
+		}
+	}()
+
+
+
+
+	//fmt.Println(querymap)
+
+
+
+
+
+
+
+
+
+
+
+
+	for {
+		time.Sleep(time.Millisecond)
 	}
-	querymap = entity.GetQueriesList(queies)
-	fmt.Println(querymap)
 
 
 
-
-
-
-
-
-	//获取stage相关详情，得到executor信息
-
-	//stagequeryurl := stagetsurl + "Inceptor::tdh2::7fd73b47-8aec-4668-951e-8c8a72255832" + "&id=10119"
-	stagequeryurl := stagetsurl + "Inceptor::tdh2::7fd73b47-8aec-4668-951e-8c8a72255832" + "&id=12447"
-	//fmt.Println(util.CrawlPage(stagequeryurl, token))
-	//fmt.Println(stagequeryurl)
-
-
-	//Get querymap info    后续切片并发需存入redis做并发访问池
-	taskmap := make(map[int]entity.Task)
-	var stage entity.JsonStage
-	err = json.Unmarshal([]byte(util.CrawlPage(stagequeryurl, token)), &stage)
-	if err != nil {
-		panic(err)
-	}
-	taskmap = entity.GeTaskList(stage)
-	fmt.Println(taskmap)
+	//
+	//
+	//
+	////获取stage相关详情，得到executor信息
+	//
+	////stagequeryurl := stagetsurl + "Inceptor::tdh2::7fd73b47-8aec-4668-951e-8c8a72255832" + "&id=10119"
+	//stagequeryurl := stagetsurl + "Inceptor::tdh2::7fd73b47-8aec-4668-951e-8c8a72255832" + "&id=12447"
+	////fmt.Println(util.CrawlPage(stagequeryurl, token))
+	////fmt.Println(stagequeryurl)
+	//
+	//
+	////Get querymap info    后续切片并发需存入redis做并发访问池
+	//taskmap := make(map[int]entity.Task)
+	//var stage entity.JsonStage
+	//err = json.Unmarshal([]byte(util.CrawlPage(stagequeryurl, token)), &stage)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//taskmap = entity.GeTaskList(stage)
+	//fmt.Println(taskmap)
 
 }
