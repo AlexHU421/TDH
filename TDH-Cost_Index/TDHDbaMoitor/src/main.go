14194dff1a864b90386f65929c3aed69cbf1beb1
AlexHU421 <31975427+AlexHU421@users.noreply.github.com>
2021-09-29 18:07:52 +0800

    Create main.go

--- TDH-Cost_Index/TDHDbaMoitor/src/main.go
+++ TDH-Cost_Index/TDHDbaMoitor/src/main.go
@@ -17,6 +17,11 @@ const(
 	stagetsurl = dbaurl + "stage" + dataKey
 	token = "XxTBdghnvfdPu2zr1NRe-TDH"
 )
+var (
+	querymaps = make(map[string]entity.Query)
+	delmaps =  make(map[string]string)
+)
+
 type MyJsonName struct {
 	Data []interface{}  `json:"data"`
 	Error []interface{} `json:"error"`
@@ -58,15 +63,12 @@ func main() {
 	fmt.Println(now)
 	//Get lastserver map info    后续切片并发需存入redis做并发访问池
 	cservermap :=make(chan map[int]entity.JsonServer)
-
 	//爬server列表（只保留最近一次启动）
 	go 	crawserverpage(cservermap)
-
 	//Test  query数据量太大，需定时器定时获取
 	cquerymap :=make(chan map[string]entity.Query)
 	//爬query列表（增量map）
-
-	go func(){
+	go func() {
 		for {
 			fmt.Println(time.Now())
 			for _, serverinfo := range <-cservermap {
@@ -74,32 +76,32 @@ func main() {
 			}
 			time.Sleep(time.Second*5)
 		}}()
-
-
-	//querymaps := make(chan map[string]entity.Query)
-	querymaps := make(map[string]entity.Query)
 	go func() {
 		for {
-			querymap := <- cquerymap
-			//querysmap :=<- querymaps
-			//fmt.Println("look", <-cquerymap)
-			for k,v :=range querymap {
-					querymaps[k]=v
+			for k,v :=range <- cquerymap {
+				querymaps[k]=v
 			}
 			for k,v :=range querymaps {
-
-				if 	util.FilterByUnixtime(v.SubmissionTime,5,"minute") {
-					delete(querymaps,k)
+				if 	util.FilterByUnixtime(v.SubmissionTime,1,"minute") {
+					delmaps[k]=k
 				}
-					fmt.Println(k,v)
 			}
+
+			fmt.Println(querymaps)
 			fmt.Println(len(querymaps))
 			time.Sleep(time.Second * 5)
 		}
 	}()
 
 
-
+	go func() {
+		for {
+			for _, v := range delmaps {
+				delete(querymaps, v)
+			}
+			time.Sleep(time.Second * 5)
+		}
+	}()
 
 	//fmt.Println(querymap)
 
@@ -115,7 +117,8 @@ func main() {
 
 
 	for {
-		time.Sleep(time.Millisecond)
+		fmt.Println("delmap:",len(delmaps),delmaps,len(delmaps))
+		time.Sleep(time.Second*5)
 	}
 
 
