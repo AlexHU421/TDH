c2fd801230aea363b12dda04033a19eb001211ea
AlexHU421 <31975427+AlexHU421@users.noreply.github.com>
2021-10-13 11:12:11 +0800

    Create server.go

---
+++ TDH-Cost_Index/TDHDbaMoitor/src/service/crawldetailspage/server.go
@@ -0,0 +1,32 @@
+package crawldetilspage
+
+import (
+	"encoding/json"
+	"strconv"
+	"tdhdbamonithr/src/entity"
+	"tdhdbamonithr/src/util"
+	"time"
+)
+
+func CrawServerPage (c chan map[int]entity.JsonServer,serverurl string,token string){
+	for {
+		dataType, _ := json.Marshal(util.JsonUnmarshalByString(util.CrawlPage(serverurl, token))["data"])
+		c <- entity.MapByJson(string(dataType))
+		time.Sleep(time.Minute)
+	}
+}
+
+func CrawServerPageFindStages (query entity.Query,querysurl string ,token string) entity.Query{
+	querystages := querysurl  + query.ServerKey + "&id=" +strconv.FormatInt(query.SqlID,10)
+	var relquery entity.JsonQueryStageInfo
+	err :=json.Unmarshal([]byte(util.CrawlPage(querystages, token)), &relquery)
+	if err != nil {
+		panic(err)
+	}
+	stagearr := make([]int64,len(relquery.Data.Stages))
+	for i :=0 ;i< len(stagearr) ;i++  {
+		stagearr[i]=relquery.Data.Stages[i].StageID
+	}
+	query.Stages=stagearr
+	return query
+}
