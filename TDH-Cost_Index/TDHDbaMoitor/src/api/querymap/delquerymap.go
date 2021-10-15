6ba4bb1d867747bf1ff517296b8d5555d76704f3
AlexHU421 <31975427+AlexHU421@users.noreply.github.com>
2021-10-15 10:13:26 +0800

    Update delquerymap.go

--- TDH-Cost_Index/TDHDbaMoitor/src/api/querymap/delquerymap.go
+++ TDH-Cost_Index/TDHDbaMoitor/src/api/querymap/delquerymap.go
@@ -1,41 +1,26 @@
 package querymap
 
 import (
-	"encoding/json"
 	"github.com/Shopify/sarama"
-	"strings"
 	"sync"
 	"tdhdbamonithr/src/entity"
+	"tdhdbamonithr/src/util"
 	"time"
 )
 
 func CleanQueryMap (querymaps map[string]entity.Query,
 					delmaps map[string]int64,
-					querymapsGuard sync.RWMutex,
+					querymapsGuard sync.Mutex,
 					delmapsGuard sync.RWMutex,
 					producer sarama.SyncProducer,
 					TopicInformation string,
+					Separator string,
 					looptime time.Duration) {
 	for {
 		for k, _ := range delmaps {
 			querymapsGuard.Lock()
 			delmapsGuard.Lock()
-			//fmt.Println("要发的")
-			//fmt.Println(querymaps[k])
-			query,_:=json.Marshal(querymaps[k])
-			//fmt.Println(
-			//	strings.Replace(
-			//		strings.Replace(
-			//			string(query),
-			//			"\n","|+---+|",-1),
-			//		"\r","|+---+|",-1))
-			//fmt.Println("发完了")
-			producer.SendMessage(&sarama.ProducerMessage{Topic:TopicInformation,Key:nil,Value: sarama.StringEncoder(
-				strings.Replace(
-				strings.Replace(
-					string(query),
-					"\n","|+---+|",-1),
-				"\r","|+---+|",-1))})
+			util.ProduceSendMsg(util.CleanNewlineChart(entity.QueryToStringBySeparator(querymaps[k],Separator)),producer,TopicInformation,)
 			delete(querymaps, k)
 			delete(delmaps,k)
 			querymapsGuard.Unlock()
