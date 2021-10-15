00b001f264ba0586aed71bc91778af4523f39155
AlexHU421 <31975427+AlexHU421@users.noreply.github.com>
2021-10-15 10:21:11 +0800

    Update kafkautil.go

--- TDH-Cost_Index/TDHDbaMoitor/src/util/kafkautil.go
+++ TDH-Cost_Index/TDHDbaMoitor/src/util/kafkautil.go
@@ -1,4 +1,11 @@
 package util
 
+import (
+	"github.com/Shopify/sarama"
+)
+
+func ProduceSendMsg (msg string,producer sarama.SyncProducer,TopicInformation string) {
+	producer.SendMessage(&sarama.ProducerMessage{Topic:TopicInformation,Key:nil,Value: sarama.StringEncoder(msg)})
 
 
+}
